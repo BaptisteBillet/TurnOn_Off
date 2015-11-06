5f070417ffa7b773af0136702baa5e620850ad40
Baptiste <b.billet@rubika-edu.com>
2015-11-06 19:13:45 +0100

    New

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class SoundManager : MonoBehaviour {
+	#region Members
+
+	[Header("VIDEOSOUND")]
+	public List<AudioClip> Music = new List<AudioClip>();
+
+	[Header("PORNSTUFF")]
+	public List<AudioClip> Porn = new List<AudioClip>();
+
+	[Header("Sound Listeners")]
+	public List<AudioSource> Source = new List<AudioSource>();
+
+
+	#endregion
+
+	// Use this for initialization
+	void Start()
+	{
+		SoundManagerEvent.onEvent += Play;
+	}
+
+	void OnDestroy()
+	{
+		SoundManagerEvent.onEvent -= Play;
+	}
+
+	public void Play(SoundManagerType emt)
+	{
+		switch (emt)
+		{
+			case SoundManagerType.ABSOLUMENT:
+				Source[2].Stop();
+				//Source[2].clip = Voice[0];
+				Source[2].Play();
+				break;
+		}
+	}
+
+	
+
+}
---
+++ Assets/VideoTest.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(AudioSource))]
+public class VideoTest : MonoBehaviour
+{
+    MovieTexture movieTexture;
+
+	
+
+	bool isSexVideoPlaying=true;
+
+    void Start ()
+    {
+		
+		StartCoroutine(StartStream());
+    }
+ 
+	void Update()
+	{
+		
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			Debug.Log("space");
+			
+			movieTexture.Stop();
+			StopAllCoroutines();
+			isSexVideoPlaying = !isSexVideoPlaying;
+			StartCoroutine(StartStream());
+			 
+		}
+		
+	}
+
+
+    protected IEnumerator StartStream ()
+    {
+		//KEEP IT HERE
+		string sexVideo = "file://" + Application.streamingAssetsPath + "/Sex1.ogv";
+		string platonicVideo = "file://" + Application.streamingAssetsPath + "/Prude1.ogv";
+		string url;
+		
+		if (isSexVideoPlaying)
+		{
+			url = sexVideo;
+		}
+		else
+		{
+			url = platonicVideo;
+		}
+		
+		WWW videoStreamer = new WWW(url);
+
+
+        movieTexture = videoStreamer.movie;
+        GetComponent<AudioSource>().clip = movieTexture.audioClip;
+        while (!movieTexture.isReadyToPlay) 
+        {
+            yield return 0;
+		}
+ 
+        GetComponent<AudioSource>().Play ();
+        movieTexture.Play ();
+        movieTexture.loop = true;
+		GetComponent<RawImage>().texture = movieTexture;
+        //GetComponent<Renderer>().material.mainTexture = movieTexture;
+    }
+}
