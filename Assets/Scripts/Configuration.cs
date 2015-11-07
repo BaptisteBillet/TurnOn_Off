9d02bb8a1560dc7fd22ec784d29f408c1a972840
Baptiste <b.billet@rubika-edu.com>
2015-11-07 12:54:37 +0100

    Change Configuration Scene

--- Assets/Scripts/Configuration.cs
+++ Assets/Scripts/Configuration.cs
@@ -9,4 +9,51 @@ public class Configuration : MonoBehaviour {
 		Application.LoadLevel("game");
 	}
 
+	void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.Alpha1))
+		{
+			configuration(1);
+		}
+		if (Input.GetKeyDown(KeyCode.Alpha2))
+		{
+			configuration(2);
+		}
+		if (Input.GetKeyDown(KeyCode.Alpha3))
+		{
+			configuration(3);
+		}
+		if (Input.GetKeyDown(KeyCode.Alpha4))
+		{
+			configuration(4);
+		}
+		if (Input.GetKeyDown(KeyCode.Alpha5))
+		{
+			configuration(5);
+		}
+
+		if (Input.GetButtonDown("A_1"))
+		{
+			configuration(3);
+		}
+		if (Input.GetButtonDown("B_1"))
+		{
+			configuration(2);
+		}
+		if (Input.GetButtonDown("X_1"))
+		{
+			configuration(4);
+		}
+		if (Input.GetButtonDown("Y_1"))
+		{
+			configuration(1);
+		}
+		if (Input.GetButtonDown("RB_1"))
+		{
+			configuration(5);
+		}
+
+	}
+
+
 }
